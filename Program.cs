3eede736ac8f194e9fbc04aa76ebbf26f7c5051d
Mac Goutell <goutellcode@gmail.com>
2015-04-09 17:36:11 -0600

    Build Two
    Looks a bit better now

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ namespace GroupPicker
         static void Main(string[] args)
         {
             List<string> listOfStudents = new List<string>(){"Mac", "Bill", "Jim", "Sally", "Chelsea", "Carla","Bob", "Tim", 
-                "Carlos","Aaron", "Dennis", "Linda"};
-            PickGroup(listOfStudents, 2);
+                "Carlos","Aaron", "Dennis", "Linda", "Jehova", "Jesus", "Abel", "Karl", "Abel", "Yoda", "Windu", "Eric"};
+            PickGroup(listOfStudents, 3);
             Console.ReadKey();
             
         }
@@ -22,6 +22,9 @@ namespace GroupPicker
             List<string> currentList = new List<string>();
             int groupNumber = 1;
             Random rng = new Random();
+            Console.WriteLine("Roger that.  I'll divide those students into groups of: " + groupSize);
+            Console.WriteLine("-----------------");
+            Console.WriteLine("-----------------");
         
             while (studentList.Count > 0)
 	        {
@@ -32,12 +35,13 @@ namespace GroupPicker
                 //if the currentGroupList size is equal to the groupSize parameter
                 if (currentList.Count == groupSize || studentList.Count == 0)
 	            {
-                    Console.WriteLine("Group " + groupNumber + " includes ");
+                    Console.WriteLine("Group " + groupNumber + " includes: ");
                     for (int i = 0; i < currentList.Count; i++)
 			        {
 			            Console.WriteLine(currentList[i]);
 			        }
-                    Console.WriteLine("______________");
+                    Console.WriteLine("-----------------");
+                    Console.WriteLine("-----------------");
                   
                     groupNumber++;
                     currentList.Clear();
